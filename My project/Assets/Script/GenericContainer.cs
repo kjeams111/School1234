3e7f60b1349746d78001070b2d095ce2aef70361
kjeams111 <kjeams111@g.shingu.ac.kr>
2023-09-07 12:07:55 +0900

    스크립트 수정

---
+++ My project/Assets/Script/GenericContainer.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenericContainer<T>
+{
+    private T[] items;
+    private int currentindex = 0;
+    
+    public GenericContainer(int capacity)
+    {
+        items = new T[capacity];
+    }
+
+    public void Add (T item)
+    {
+        if(currentindex <  items.Length)
+                {
+            items[currentindex] = item;
+            currentindex++;
+        }
+    }
+
+    public T[] GetItems()
+    {
+        return items;
+    }
+   
+    
+}
---
+++ My project/Assets/Script/GenericContainerExample.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenericContainerExample : MonoBehaviour
+{
+
+    private GenericContainer<int> intContainer;
+    private GenericContainer<string> stringContainer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        intContainer = new GenericContainer<int>(10);
+        stringContainer = new GenericContainer<string>(5);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            intContainer.Add(Random.Range(0, 100));
+            DisplayContaineritems(intContainer);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            string randomString = "item" + Random.Range(0, 100);
+            stringContainer.Add(randomString);
+            DisplayContaineritems(intContainer);
+        }
+    }
+
+
+    private void DisplayContaineritems<T>(GenericContainer<T> container)
+    {
+        T[] item = container.GetItems();
+        string temp = "";
+        for (int i = 0; i < item.Length; i++)
+        {
+            if (item[i] != null)
+            {
+                temp += item[i].ToString() + "/";
+
+            }
+
+            else
+            {
+                temp += "Empty /";
+            }
+        }
+            Debug.Log(temp);
+    }
+}
+
+
+
