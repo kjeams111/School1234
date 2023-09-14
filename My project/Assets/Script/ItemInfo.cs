892bafc3136425cc60a5feb2bf866df34da63eaa
kjeams111 <kjeams111@g.shingu.ac.kr>
2023-09-14 10:59:10 +0900

    Item
    아이템 스크립트

---
+++ My project/Assets/Script/ItemInfo.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemInfo : MonoBehaviour
+{
+    public int slotld;
+    public int itemld;
+
+    public void InitDummy(int slotid, int itemld)
+    {
+        this.slotld = slotld;
+        this.slotld = itemld;
+
+    }
+}
---
+++ My project/Assets/Script/item.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class item : MonoBehaviour
+{
+    public int id;
+
+   // public void init(int id, Slot slot)
+    //{
+
+        //public void init(int id, Slot slot
+        //{
+        //    this.id = id;
+        //    this.parentSlor = slot;
+        // }
+
+   // }
+}
